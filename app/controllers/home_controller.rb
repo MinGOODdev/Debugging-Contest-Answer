f4921a14912a47e7415a6f57086eaa230ef748f8
hyesungoh <haesungoh414@gmail.com>
2018-06-22 09:29:11 +0000

    기본 베이스

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,64 @@
+class HomeController < ApplicationController
+  def index
+    if user_signed_in?
+      @posts = Post.all.reverse
+    else
+      redirect_to '/users/sign_in'
+    end
+  end
+
+  def new
+  end
+  
+  def create
+    @post = Post.new
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
+    @post.user_id = current_user.id
+    
+      hashtags = params[:hashtags].split(',')
+      hashtags.each do |tag|
+        hashtag = Hashtag.find_or_create_by(name: tag.delete('#'))
+        hashtag.save
+        @post.hashtags << hashtag
+      end
+      
+    uploader = ImguploaderUploader.new
+    uploader.store!(params[:img])
+    @post.img_url = uploader.url
+    
+    @post.save
+    
+    redirect_to '/home/index'
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+  end
+  
+  def update
+    @post = Post.find(params[:post_id])
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
+    @post.user_id = current_user.id
+    @post.hashtags.clear
+      
+      hashtags = params[:hashtags].split(',')
+      hashtags.each do |tag|
+        hashtag = Hashtag.find_or_create_by(name: tag.delete('#'))
+        hashtag.save
+        @post.hashtags << hashtag
+      end
+    
+    @post.save
+    
+    redirect_to '/home/index'
+  end
+  
+  def delete
+    post = Post.find(params[:post_id])
+    post.destroy
+    
+    redirect_to '/home/index'
+  end
+end
